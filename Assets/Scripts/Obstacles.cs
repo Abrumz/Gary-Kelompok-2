f8c1b5d0e41685cb38160825d0c04f3aac25960a
generasirabbani <generasirabbani@apps.ipb.ac.id>
2022-06-23 00:23:30 +0700

    initial commit

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+
+    public float lifetime;
+    public float secBeforeDeath;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(player.gameObject, secBeforeDeath);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed;
+    float score;
+
+    public Text ScoreText;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    [SerializeField]
+    bool isGrounded = false;
+    bool isAlive = true;
+
+    Rigidbody2D RB;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+        if (isAlive)
+        {
+            score += Time.deltaTime * 5;
+            ScoreText.text = "SCORE : " + score.ToString("F");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move Our Character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if(isGrounded == false)
+            {
+                isGrounded = true;
+            }
+        }
+
+        if (collision.gameObject.CompareTag("Obstacles"))
+        {
+            isAlive = false;
+            Time.timeScale = 0;
+        }
+    }
+}
